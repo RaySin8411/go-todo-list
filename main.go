9a90d3a9ca0a156ca0a08e79a5863f84d56a0329
raysin8411 <q8977452@gmail.com>
2026-03-06 10:34:02 +0800

    (develop) add get /todos and /todos/:id

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
@@ -40,6 +41,31 @@ func main() {
 		todos = append(todos, newTodo)
 
 		return c.JSON(http.StatusOK, newTodo)
+	})
+
+	// 列出全部任務：GET /todos
+	e.GET("/todos", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, todos)
+	})
+
+	// 查單一任務：GET /todos/:id
+	e.GET("/todos/:id", func(c echo.Context) error {
+		idStr := c.Param("id")
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]string{
+				"error": "id must be a number",
+			})
+		}
+
+		for _, t := range todos {
+			if t.ID == id {
+				return c.JSON(http.StatusOK, t)
+			}
+		}
+		return c.JSON(http.StatusNotFound, map[string]string{
+			"error": "todo not found",
+		})
 	})
 
 	// 啟動伺服器，監聽在 http://localhost:1323
